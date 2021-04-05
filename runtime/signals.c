32d7789d59293e457e0d5bfc21dace92c6f96d22
Gerd Stolpmann <gerd@figlyinc.com>
2021-04-05 21:30:09 +0200

    fixup: Sys.catch_break (signals)

--- runtime/signals.c
+++ runtime/signals.c
@@ -453,6 +453,9 @@ CAMLprim value caml_install_signal_handler(value signal_number, value action)
   int sig, act, oldact;
 
   sig = caml_convert_signal_number(Int_val(signal_number));
+#ifdef CAML_USE_WASICAML
+  if (Int_val(signal_number) == -6) sig = 1;  // just ignore this one (Sys.catch_break)
+#endif
   if (sig < 0 || sig >= NSIG)
     caml_invalid_argument("Sys.signal: unavailable signal");
   switch(action) {
